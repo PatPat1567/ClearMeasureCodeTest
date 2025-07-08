2b329b811de28cd19efc31763e9cb9548ca61839
Patrick <towle_patrick@yahoo.com>
2025-07-08 10:28:06 -0500

    Clean up performance and leave a blurb relating to perforamnce loss for large numbers

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,17 +2,23 @@
 {
     public class Class1 : IClass1
     {
+
+        /** In theory, this solution would work for all numbers up to int.MaxValue, which is 2,147,483,647.
+            However, due to performance of machines, this is an impractical maximum. This solution should work 
+            up to 100 thousand (100,000) without significant notice of the performance loss. If the customer
+            did need to produce lists over that 100 thousand mark, we would need to rewrite this to allow for 
+            paging, where they would be able to request how many on the result set and we could call into this
+            method with a change in the lower and upper bounds to provide for those pages. 
+        **/
         public ICollection<string> PrintValue(int start, int max, List<Tuple<int, string>> rules)
         {
-            IDictionary<int, string> printedValues = new Dictionary<int, string>();
+            IList<string> printedValues = [];
             if (rules is null)
             {
                 rules = new List<Tuple<int, string>>();
             }
             for (var x = start; x <= max; x++)
             {
-                // Add unit tests
-                // Clean up so it doesn't hang when passed int.max
                 var ruleValue = "";
                 foreach (var rule in rules)
                 {
@@ -25,10 +31,10 @@
                 {
                     ruleValue = x.ToString();
                 }
-                printedValues.Add(x, ruleValue);
+                printedValues.Add(ruleValue);
             }
 
-            return printedValues.Values;          
+            return printedValues;          
                 
         }
     }
