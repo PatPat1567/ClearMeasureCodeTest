9ba92f06703c40baf0b06f920db3bd35d7d3baea
Patrick Towle <towle_patrick@yahoo.com>
2025-07-08 00:24:43 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1 : IClass1
+    {
+        public IDictionary<int, string> PrintValue(int start, int max, List<Tuple<int, string>> rules)
+        {
+            IDictionary<int, string> printedValues = new Dictionary<int, string>();
+            for (var x = start; x <= max; x++)
+            {
+                var ruleValue = "";
+                foreach (var rule in rules)
+                {
+                    if (x % rule.Item1 == 0)
+                    {
+                        ruleValue += rule.Item2;
+                    }
+                }
+                if (string.IsNullOrEmpty(ruleValue))
+                {
+                    ruleValue = x.ToString();
+                }
+                printedValues.Add(x, ruleValue);
+            }
+
+            return printedValues;          
+                
+        }
+    }
+}
---
+++ ClassLibrary1/IClass1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public interface IClass1
+    {
+        IDictionary<int, string> PrintValue(int start, int max, List<Tuple<int, string>> rules);
+    }
+}
